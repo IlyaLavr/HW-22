9e3fe9f37923597226610ffed2da331ab498a08d
Ilya <iilavr@mail.ru>
2022-12-01 14:07:22 +0300

    Initial Commit

---
+++ HW-22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-22
+//
+//  Created by Илья on 01.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
